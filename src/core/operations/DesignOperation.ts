476ae5363b6f9b58fb894d74d93eb41ecf65d377
88b997hs48-ai <88b997hs48@privaterelay.appleid.com>
2026-08-30 13:22:14 -0600

    Create DesignOperation.ts

---
+++ src/core/operations/DesignOperation.ts
@@ -0,0 +1,50 @@
+import { DesignState, FlyComponent } from ‘../design/types’;
+
+export type DesignOperation =
+| {
+type: ‘ADD_COMPONENT’;
+component: FlyComponent;
+}
+| {
+type: ‘REMOVE_COMPONENT’;
+componentId: string;
+}
+| {
+type: ‘UPDATE_COMPONENT’;
+componentId: string;
+updates: Partial;
+};
+
+export function applyOperationToSnapshot(
+snapshot: DesignState,
+operation: DesignOperation,
+): DesignState {
+const nextState = structuredClone(snapshot);
+
+switch (operation.type) {
+case ‘ADD_COMPONENT’: {
+nextState.components.push(structuredClone(operation.component));
+return nextState;
+}
+
+case 'REMOVE_COMPONENT': {
+  nextState.components = nextState.components.filter(
+    (component) => component.id !== operation.componentId,
+  );
+  return nextState;
+}
+case 'UPDATE_COMPONENT': {
+  const component = nextState.components.find(
+    (candidate) => candidate.id === operation.componentId,
+  );
+  if (!component) {
+    throw new Error(
+      `Component '${operation.componentId}' was not found.`,
+    );
+  }
+  Object.assign(component, structuredClone(operation.updates));
+  return nextState;
+}
+
+}
+}
